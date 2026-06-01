139aeaf3b5427cac6ebbdc253d044b2ea9c9ae37
krasotun <servrostov@gmail.com>
2026-06-01 15:18:45 +0300

    add visual workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2e',
   globalSetup: './e2e/container/global-setup.ts',
+  globalTeardown: './e2e/container/global-teardown.ts',
   use: {
     baseURL: 'http://localhost:8080',
     trace: 'on-first-retry',
